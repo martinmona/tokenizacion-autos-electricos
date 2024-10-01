d89d214c01f6ce7c2ed9b3547ac547ae538d08a7
martinmona <martin.m94@hotmail.com>
2024-10-01 19:39:06 -0400

    create hardhat project and build success

---
+++ core/hardhat.config.ts
@@ -0,0 +1,24 @@
+import {env} from "process";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.25",
+  networks: {
+    hardhat: {},
+    amoy: {
+      url: RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
+
+export default config;
