e43d8a28379b39df7775ecfd1563bc06fdfd4c48
martinmona <martin.m94@hotmail.com>
2024-10-26 16:42:20 -0300

    transfer nft on sale, set max supply

--- core/contracts/CarContract.sol
+++ core/contracts/CarContract.sol
@@ -6,56 +6,102 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CarContract is ERC721, Ownable {
-  uint256 public immutable MAX_SUPPLY;
-  uint256 internal _numAvailableRemainingTokens;
-  mapping(uint256 => Car) public cars;
-  uint256 internal _tokenIdCounter = 0;
+    uint256 public immutable MAX_SUPPLY;
+    uint256 internal _numAvailableRemainingTokens;
+    mapping(uint256 => Car) private cars;
+    uint256 private _tokenIdCounter = 0;
+    enum CarState {
+        Presale,
+        InSale,
+        Sold
+    }
 
+    struct Car {
+        string brand;
+        string model;
+        uint256 year;
+        uint256 price;
+        uint256 kilometers;
+        CarState state;
+    }
 
-  struct Car {
-    string brand;
-    string model;
-    uint256 year;
-    uint256 price;
-    uint256 kilometers;
-    address owner;
-    string state; // Presale, In sale, Sold
-  }
+    event CarStateChanged(uint256 carId, CarState newCarState);
+    event CarCreated(Car newCar);
 
-  event CarStateChanged(uint256 carId, string value);
-  event CarCreated(Car newCar);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 maxSupply
+    ) ERC721(_name, _symbol) Ownable(msg.sender) {
+        MAX_SUPPLY = maxSupply;
+    }
 
-  constructor(
-      string memory _name,
-      string memory _symbol
-  ) ERC721(_name, _symbol) Ownable(msg.sender) {}
+    receive() external payable {
+        revert("This contract does not accept Ether");
+    }
 
-  function createCar(string memory brand, string memory model, uint256 year, uint256 price) public onlyOwner returns (uint256) {
-    Car memory car = Car(brand, model, year, price, 0, msg.sender, "Presale");
-    uint256 carId = _tokenIdCounter;
-    cars[carId] = car;
-    _safeMint(msg.sender, carId);
-    emit CarCreated(car);
-    _tokenIdCounter++;
-    return carId;
-  }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        require(cars[tokenId].state == CarState.InSale, "Car not for sale");
+        super.transferFrom(from, to, tokenId);
+    }
 
-  function getCar(uint256 carId) public view returns (Car memory) {
-    return cars[carId];
-  }
+    function createCar(
+        string memory brand,
+        string memory model,
+        uint256 year,
+        uint256 price
+    ) public onlyOwner returns (uint256) {
+        require(year >= 1900, "Year must be greater than 1900");
+        require(_tokenIdCounter < MAX_SUPPLY, "Max supply reached");
+        Car memory car = Car(
+            brand,
+            model,
+            year,
+            price,
+            0,
+            CarState.Presale
+        );
+        uint256 carId = _tokenIdCounter;
+        cars[carId] = car;
+        _tokenIdCounter += 1;
+        _safeMint(msg.sender, carId);
+        emit CarCreated(car);
+        return carId;
+    }
 
-  function putCarOnSale(uint256 carId, uint256 price, uint256 kilometers) public {
-    require(cars[carId].owner == msg.sender, "You are not the owner of this car");
-    cars[carId].price = price;
-    cars[carId].kilometers = kilometers;
-    cars[carId].state = "In sale";
-    emit CarStateChanged(carId, "In sale");
-  }
+    function getCar(uint256 carId) public view returns (Car memory) {
+        return cars[carId];
+    }
 
-  function sellCar(uint256 carId, address newOwner) public {
-    require(cars[carId].owner == msg.sender, "You are not the owner of this car");
-    cars[carId].state = "Sold";
-    emit CarStateChanged(carId ,"Sold");
-    cars[carId].owner = newOwner;
-  }
+    function putCarOnSale(
+        uint256 carId,
+        uint256 price,
+        uint256 kilometers
+    ) public {
+        require(
+            ownerOf(carId) == msg.sender,
+            "You are not the owner of this car"
+        );
+        require(cars[carId].state == CarState.Presale, "Car is already on sale");
+        require(cars[carId].kilometers >= kilometers, "The car can't have less kilometers");
+        cars[carId].price = price;
+        cars[carId].kilometers = kilometers;
+        cars[carId].state = CarState.InSale;
+        emit CarStateChanged(carId, CarState.InSale);
+    }
+
+    function sellCar(uint256 carId, address newOwner) public {
+        require(
+            ownerOf(carId) == msg.sender,
+            "You are not the owner of this car"
+        );
+        require(cars[carId].state == CarState.InSale, "Car is not on sale");
+        safeTransferFrom(msg.sender, newOwner, carId);
+        cars[carId].state = CarState.Sold;
+        emit CarStateChanged(carId, CarState.Sold);
+    }
 }
--- core/scripts/deploy.ts
+++ core/scripts/deploy.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 async function main() {
   const CarContract = await ethers.getContractFactory("CarContract");
   console.log("Deploying Car...");
-  const carContract = await CarContract.deploy("Car", "CAR");
+  const carContract = await CarContract.deploy("Car", "CAR", 2000);
   await carContract.waitForDeployment();
   console.log("carContract deployed to:", await carContract.getAddress());
 }
